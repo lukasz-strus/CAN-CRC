99ecde1997a57a3799c46b135c0d108e058e9bbf
Łukasz Strus <61932823+lukasz-strus@users.noreply.github.com>
2024-06-10 19:12:03 +0200

    Fix big

--- CAN-CRC/Models/CRC.cs
+++ CAN-CRC/Models/CRC.cs
@@ -3,15 +3,19 @@ namespace CAN_CRC.Models;
 // ReSharper disable InconsistentNaming
 internal class CRC
 {
-    internal int Calculate(int[] data)
+    public int Calculate(int[] data)
     {
-        var crc = 0;
+        short crc = 0;
         foreach (var current in data)
         {
-            crc ^= current << 7;
+            crc ^= (short)(current << 7);
             for (var i = 0; i < 8; i++)
             {
-                crc = (crc << 1) ^ ((crc & 0x8000) != 0 ? 0x4599 : 0);
+                crc <<= 1;
+                if ((crc & 0x8000) != 0)
+                {
+                    crc ^= 0x4599;
+                }
             }
             crc &= 0x7FFF;
         }
